465d6ae4b9ce4c525cc2a480d15cfd4be8955a99
Muhammad Maaz <35654937+maazu@users.noreply.github.com>
2020-12-03 01:49:54 +0500

    Update validate_urls.py

--- validate_urls.py
+++ validate_urls.py
@@ -1,25 +1,34 @@
-
 # modified fetch function with semaphore
-import random
+
 import asyncio
 from aiohttp import ClientSession
 import pandas as pd
 import time
-import pprint
 import json 
 import os
 import glob
 
 start_time = time.time()
-response_dict = {}
+sucess_dict = {}
 error_dict = {}
+c = (
+    "\033[0m",   # End of color
+    "\033[36m",  # Cyan
+    "\033[91m",  # Red
+    "\033[35m",  # Magenta
+)
+
+
+def generated_time():
+    geneerated_time = time.strftime("%Y-%m-%d-%H-%M-%S")
+    return geneerated_time
 
 
 def select_csv():
      confirm_dataset = input("Enter 'y' to confirm dataset upload: ")
      if (confirm_dataset == "y"):
         try:
-          directory  = os.getcwd() + "/"
+          directory  = os.getcwd() + "/csv-script/"
           uploaded_csv = glob.glob(directory + "*.csv")
           if (len(uploaded_csv) > 0):
             count = 0
@@ -48,11 +57,6 @@ def read_website_df_single():
     return df
 
 
-def generated_time():
-    geneerated_time = time.strftime("%Y-%m-%d-%H-%M-%S")
-    return geneerated_time
-
-
 
 def create_new_directory(location,folder_name):
   
@@ -62,18 +66,20 @@ def create_new_directory(location,folder_name):
     print(str(folder_name) + " directory created: " + str(download_dir))  
     return path
 
+
 def listToString(s):  
     str1 = "," 
     return (str1.join(s)) 
 
+
 def add_summary_to_df(sum_dictionary,file_name,type_of_file):
   
-    df = pd.DataFrame(list(sum_dictionary.items()),columns = ['hostname','server respone']) 
+    df = pd.DataFrame(list(sum_dictionary.items()),columns = ['hostname',"store",'server respone']) 
     df['server respone'] = df['server respone'].apply(listToString)
     df = df.join(df['server respone'].str.split(',', expand=True).add_prefix('Response '))
     df = df.drop('server respone', 1)
-    if(type_of_file == "error"):
-         df.rename(columns={'Response 0':  'store'}, inplace=True)
+    
+    df.rename(columns={'Response 0':  'store'}, inplace=True)
     df.rename(columns={'Response 0':  'endpoint'}, inplace=True)
     df.rename(columns={'Response 1': 'status'}, inplace=True)
     df.rename(columns={'Response 2': 'type'}, inplace=True)
@@ -81,82 +87,105 @@ def add_summary_to_df(sum_dictionary,file_name,type_of_file):
 
 
 
-async def update_dictionary(hostname,response_url,status,contentType,delay,date): 
+async def update_dictionary(hostname,store_url,response_url,status,contentType,delay,date): 
+    
     if(contentType == "application/json" or contentType ==  "application/json; charset=utf-8"):
-        response_dict[hostname] = [str(response_url), str(status),str(contentType)]
+        sucess_dict[hostname] = [str(store_url),str(response_url), str(status),str(contentType)]
+        print(c[0]+ "{} {}:{} with delay {} {}".format(status, contentType, date, response_url, delay,status))  
     else:
-        error_dict[hostname] = [str(response_url), str(status),str(contentType)]
-
-    print("{} {}:{} with delay {} {}".format(status, contentType, date, response_url, delay,status))  
-
-
+        error_dict[hostname] = [str(store_url), str(response_url), str(status),str(contentType)]
+        print(c[1]+ "{} {}:{} with delay {} {}".format(status, contentType, date, response_url, delay,status))  
+   
 
 
-async def fetch(url, session):
+
+
+async def fetch(hostname,store, session):
     try:
-        hostname = url[8:-31]
-        async with session.get(url) as response:
-           delay = response.headers.get("DELAY")
-           date = response.headers.get("DATE")
-           status = response.status
-           contentType = response.headers.get('content-type')
-           await update_dictionary(hostname,response.url,status,contentType,delay,date)
-           return await response.read()
+        store_url = store[8:-31]
+        try:  
+            async with session.get(store) as response:
+                await asyncio.sleep(1)
+                delay = response.headers.get("DELAY")
+                date = response.headers.get("DATE")
+                status = response.status
+                contentType = response.headers.get('content-type')
+                  
+                await update_dictionary(hostname,store_url,response.url,status,contentType,delay,date)
+         
+        except Exception as e:
+            error_dict[hostname]  = [store,store_url,"NOT FOUND","NOT FOUND"]
+            status = "000"
+            print("{} NOT FOUND {}".format(status, store))
+            print(e)
+            pass 
+            
+            # yield control for 1 second
+           
     except Exception as e:
-        error_dict[hostname]  = [str(url),"NOT FOUND","NOT FOUND"]
+        error_dict[hostname]  = [store,store_url,"NOT FOUND","NOT FOUND"]
         status = "000"
-        print("{} NOT FOUND {}".format(status, url))
-        return status
+        print("{} NOT FOUND {}".format(status, store))
+        return e
       
 
-async def bound_fetch(sem, url, session):
-    
-    # Getter function with semaphore.
+async def bound_fetch(sem, hostname,store, session):
+
     async with sem:
-        await fetch(url, session)
+        await fetch(hostname,store, session)
 
 
-async def run(store_list): 
+async def run(host_name_list,store_list): 
     tasks = []
-    # create instance of Semaphore
+  
     sem = asyncio.Semaphore(1000)
-
-    # Create client session that will ensure we dont open new connection
-    # per each request.
     async with ClientSession() as session:
-        for url in store_list:
-            # pass Semaphore and session to every GET request
-            task = asyncio.ensure_future(bound_fetch(sem, url, session))
-            tasks.append(task)
-
-        responses = asyncio.gather(*tasks)
-        await responses
+      for (hostname,store) in zip(host_name_list,store_list):
+        try:
+          task = asyncio.ensure_future(bound_fetch(sem, hostname,store, session))
+          await asyncio.sleep(1)
+          tasks.append(task)
+        except Exception as e:
+          print(e)
+      responses = asyncio.gather(*tasks)
+      await responses
     return task
    
 
 
-
+def main(host_name_list,store_list):
+    
+    loop = asyncio.get_event_loop()
+    future = asyncio.ensure_future(run(host_name_list,store_list))
+    
+    loop.run_until_complete(future)
       
 if __name__ == "__main__": 
-    choice = input("File type:\nEnter 'e' to verify csv\nEnter 'a' for normal run  ===> ")
+    download_directory  = os.getcwd() + "/generated-files/"
+    summary_dir  = create_new_directory(download_directory,"validation-summary-"+generated_time())
+    
+    choice = input("File type:\nEnter 'e' to verify the error csv file \nEnter 'a' for normal run  ===> ")
+    
     df =  read_website_df_single()
+    
     if(choice == "e"):
+         host_name_list = list(df['hostname'])               
          store_list = list(df['store'])
-    else:                       
+    else:  
+        host_name_list = list(df['hostname'])                     
         store_list = list(df['store'].apply(create_shopify_api_endpoint_for_store))
     
     print("Total Urls =========> " +str( len(store_list)))
     
-    loop = asyncio.get_event_loop()
-    future = asyncio.ensure_future(run(store_list))
-    loop.run_until_complete(future)
     
-    print("---Request Compelete total time ==> : {0:.3g} seconds ---".format (time.time() - start_time))
+    main(host_name_list,store_list)
+    
+
     print("---Creating Validation Summary ---")
    
-    summary_dir  = create_new_directory("","validation-summary-"+generated_time())
-    add_summary_to_df(response_dict,summary_dir+"/valid-endpoints.csv","valid")    
+    
+    add_summary_to_df(sucess_dict,summary_dir+"/valid-endpoints.csv","valid")    
     add_summary_to_df(error_dict,summary_dir+ "/error-urls.csv","error")
     
-    print("--- Validation Summary Folder Created  ---")
+    print("--- Validation Summary Folder created  ---")
     print("---Script total time ==> : {0:.3g} seconds ---".format (time.time() - start_time))
